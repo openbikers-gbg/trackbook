c4730bbcab24dc0488f139be396a790edc7fb97f
Arianna Masciolini <uzkamascio@gmail.com>
2019-05-11 20:15:21 +0200

    Work in progress on recyclerview.

--- app/src/main/java/org/y20k/trackbook/MainActivityTrackFragment.kt
+++ app/src/main/java/org/y20k/trackbook/MainActivityTrackFragment.kt
@@ -6,50 +6,75 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
 
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.LinearLayoutManager
+import org.y20k.trackbook.core.Track
+
+import org.y20k.trackbook.helpers.StorageHelper
+import org.y20k.trackbook.helpers.LogHelper
+import java.io.File
+import android.R
+import kotlinx.android.synthetic.main.list_item.*
+import android.graphics.Movie
+
+
+
 
 
 class MainActivityTrackFragment : Fragment(), TrackbookKeys {
-    // dumb data to populate the recyclerView
-    var strings = arrayOf("route1", "route2", "route3", "route4", "route5", "route6", "route7")
+    var tracks = mutableListOf<Track>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rv = RecyclerView(context!!)
         rv.layoutManager = LinearLayoutManager(context)
-        rv.adapter = RVAdapter(strings)
+        val storageHelper = StorageHelper(context)
+        var files = storageHelper.listOfTrackbookFiles
+        for (file in files) {
+            val track = storageHelper.loadTrack(file)
+            tracks.add(track)
+        }
+        rv.adapter = RVAdapter(tracks)
         return rv
     }
 
     /**
      * Adapter for the RecyclerView
      */
-    inner class RVAdapter(private val dataSource: Array<String>) : RecyclerView.Adapter<ViewHolder>() {
+    inner class RVAdapter(private val trackList: List<Track>) : RecyclerView.Adapter<ViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = TextView(parent.context)
+            val view = LinearLayout(parent.context)
+            LogHelper.v("viewis:", view.toString())
             return ViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.textView.text = dataSource[position]
+            val track = trackList.get(position)
+            holder.routeName?.setText("Track")
+            holder.date?.setText(track.recordingStart.toString())
+            holder.distance?.setText(track.trackDistance.toString())
         }
 
         override fun getItemCount(): Int {
-            return dataSource.size
+            return trackList.size
         }
     }
 
     /**
      * ViewHolder for the RecyclerView
      */
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var textView: TextView
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var routeName = RouteName
+        var date = Date
+        var distance = Distance
 
-        init {
-            textView = itemView as TextView
-        }
+        /*init {
+            var routeName = RouteName as TextView
+            var date = Date as TextView
+            var distance = Distance as TextView
+        }*/
     }
 }
